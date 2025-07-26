fefd08b33970756007858e4a638e480e520fc1e4
Revoj-a <revojjoever@gmail.com>
2025-07-26 21:51:10 +0800

    fifth commit

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ function getRGBComponents(rgbString) {
   const result = rgbString.match(/\d+/g).map(Number);
   return { r: result[0], g: result[1], b: result[2] };
 }
+
 function getLuminance(r, g, b) {
   return 0.299 * r + 0.587 * g + 0.114 * b;
 }
